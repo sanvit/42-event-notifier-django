9ee8bd059b18de822420c9c8f42f36985e161bef
Jaewon Kim <sanvit@sanvit.me>
2021-12-31 17:23:47 +0000

    register models to admin panel

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+admin.site.register(Event)
+admin.site.register(Theme)
+admin.site.register(Campus)
+admin.site.register(Cursus)
